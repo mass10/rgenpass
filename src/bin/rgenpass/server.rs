e60653c8fb4a000d3a2d577bcb0f74f27aef188e
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-08 23:32:13 +0900

    Create server.rs

---
+++ src/bin/rgenpass/server.rs
@@ -0,0 +1,13 @@
+use super::util;
+
+// サーバーの起動を試みます。
+pub fn spawn_server_process() -> Result<(), Box<dyn std::error::Error>> {
+	let executing_app_path = util::get_runnning_path()?;
+	let mut command = std::process::Command::new(&executing_app_path);
+	command.arg("--server");
+	let result = command.spawn();
+	if result.is_err() {
+		println!("[ERROR] {} [{}]", result.err().unwrap(), &executing_app_path);
+	}
+	return Ok(());
+}

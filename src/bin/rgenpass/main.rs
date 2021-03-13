bc65482c6efd2e302adf3c51ab51366aa527b481
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-13 11:34:08 +0900

    Update main.rs

--- src/bin/rgenpass/main.rs
+++ src/bin/rgenpass/main.rs
@@ -1,33 +1,18 @@
+extern crate crossterm;
+
 mod application;
-mod application_error;
-mod client;
 mod generator;
-mod listener;
-mod server;
 mod util;
 
-/// Usage.
-fn usage() {
-	println!("No usage.");
-}
-
-/// アプリケーションのエントリーポイント
+///
+/// Application entrypoint.
+///
 fn main() {
-	let mut request = "".to_string();
-	for e in std::env::args().skip(1) {
-		if e == "--help" {
-			usage();
-			return;
-		}
-		if e == "--server" {
-			request = "--server".to_string();
-		}
-	}
-
-	// アプリケーションを起動
-	let result = application::run(&request);
+	// Run application.
+	let result = application::run();
 	if result.is_err() {
-		println!("[ERROR] {}", result.err().unwrap());
+		let error = result.err().unwrap();
+		println!("[ERROR] {}", error);
 		return;
 	}
 }

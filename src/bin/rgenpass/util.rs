874486928e80f8339e98263b9b9e65afd01638b5
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-14 12:06:31 +0900

    Update util.rs

--- src/bin/rgenpass/util.rs
+++ src/bin/rgenpass/util.rs
@@ -2,42 +2,49 @@
 /// Time keeper
 ///
 pub struct TimeKeeper {
+	/// Start time
 	start: Option<std::time::Instant>,
-	times: u32,
 }
 
 impl TimeKeeper {
+	/// Returns a new instance.
+	///
+	/// ### Returns
+	/// A new instance of TimeKeeper
 	pub fn new() -> TimeKeeper {
-		return TimeKeeper { start: None, times: 0 };
+		return TimeKeeper { start: None };
 	}
 
+	/// Start keeper
 	pub fn start(&mut self) {
 		self.start = Some(std::time::Instant::now());
 	}
 
 	/// Check time keeper started or not.
 	///
-	///
+	/// ### Returns
+	/// `true` if started.
 	fn started(&mut self) -> bool {
 		return self.start.is_some();
 	}
 
-	/// Check elapsed.
+	/// Check elapsed time.
 	///
-	/// # Returns
-	/// true when time over.
+	/// ### Returns
+	/// `true` when it's over.
 	pub fn is_over(&mut self) -> bool {
 		if !self.started() {
 			// Not started.
 			return false;
 		}
 
-		self.times += 1;
 		let current_time = std::time::Instant::now();
 		let erapsed = current_time - self.start.unwrap();
-		if erapsed.as_millis() < 600 {
+		if erapsed.as_millis() < 700 {
 			return false;
 		}
+
+		// It's over
 		return true;
 	}
 }
